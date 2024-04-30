53b624b32bd1ca3fc285d83fe41122a4250a40c6
Lein0rb4gX <lein0rb4g@gmail.com>
2024-04-30 20:41:37 -0300

    add new tailwind classes and also extends so can do the correct design pattern modified:   news-homepage-main/css/input.css modified:   news-homepage-main/index.html modified:   news-homepage-main/style.css modified:   news-homepage-main/tailwind.config.js

--- news-homepage-main/tailwind.config.js
+++ news-homepage-main/tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
 			colors: {
 				"soft-red": "hsl(5, 85%, 63%)",
 				"soft-orange": "hsl(35, 77%, 62%)",
+				"off-white": "hsl(36, 100%, 99%)",
+				"grayish-blue": "hsl(233, 8%, 50%)",
+				"dark-grayish": "hsl(236, 13%, 42%)",
 				"very-dark-blue": "hsl(240, 100%, 5%)",
+			},
+			fontFamily: {
+				inter: ["Inter", "sans-serif"], // Add "sans-serif" as fallback
 			},
 		},
 	},
