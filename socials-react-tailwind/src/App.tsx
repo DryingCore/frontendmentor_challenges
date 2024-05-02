fbdf3fc2c3bc8a511b141b043c120ebd6280e23b
Gabriel Antunes Rocha <137729079+DryingCore@users.noreply.github.com>
2024-05-02 16:55:17 -0300

    Update App.tsx

--- socials-react-tailwind/src/App.tsx
+++ socials-react-tailwind/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <div className="flex justify-center items-center h-screen bg-black">
           <div className="bg-card text-center text-white w-[25rem] h-[43rem] rounded-xl">
             <Description avatar={profilePicture} alt={"Profile Picture"} width={90} />
-            <Button link={"https://dryingcore.github.io/socials/"} desc={"GitHub"} />
+            <Button link={"https://github.com/DryingCore"} desc={"GitHub"} />
             <Button link={"https://www.linkedin.com/in/gabriel-antunes-rocha-816b482a6/"} desc={"LinkedIn"} />
             <Button link={"https://www.instagram.com/__antunesgabriel/"} desc={"Instagram"} />
             <Button link={"https://ind3x.games/"} desc={"Ind3x Interactive"} />
